dd950e3f7b856ba343efca81287b2d411371b3e0
lttb <kenzhaev.artur@gmail.com>
2019-06-25 12:48:07 +0300

    build(react): fix package build

--- packages/react/babel.config.js
+++ packages/react/babel.config.js
@@ -9,5 +9,5 @@ module.exports = {
     ],
     '@babel/preset-react',
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
 };
